66d775f032c01e240bb3c0b41e80789ee5431e40
sebas <sebaslpez0519@gmail.com>
2021-11-24 09:59:51 -0500

    finish funtionality crud

--- src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -1,7 +1,38 @@
 package co.com.sofka.crud.controllers;
 
+import co.com.sofka.crud.Enities.Todo;
+import co.com.sofka.crud.repositories.TodoRepository;
+import co.com.sofka.crud.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> list(){
+        return todoService.list();
+    }
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
+        return todoService.save(todo);
+    }
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if (todo.getId()!=null){
+            return todoService.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualizar");
+    }
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id){
+        todoService.delete(id);
+    }
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id){
+        return todoService.get(id);
+    }
 }
--- src/main/java/co/com/sofka/crud/services/TodoService.java
+++ src/main/java/co/com/sofka/crud/services/TodoService.java
@@ -1,7 +1,25 @@
 package co.com.sofka.crud.services;
 
+import co.com.sofka.crud.Enities.Todo;
+import co.com.sofka.crud.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public Iterable<Todo> list(){
+        return todoRepository.findAll();
+    }
+    public Todo save(Todo todo){
+        return todoRepository.save(todo);
+    }
+    public void delete(Long id){
+        todoRepository.delete(get(id));
+    }
+    public Todo get(Long id){
+        return todoRepository.findById(id).orElseThrow();
+    }
 }
